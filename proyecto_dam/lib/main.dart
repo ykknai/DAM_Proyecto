a02960c8db0e8a5fd224fee0b6a73fcacd86832c
ykknai <luis.saavedra0800@gmail.com>
2025-11-19 11:30:30 -0300

    cambio
    Removed commented code for clarity.

--- proyecto_dam/lib/main.dart
+++ proyecto_dam/lib/main.dart
@@ -13,7 +13,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,9 +25,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/auth',
 
       routes: {
-        '/auth': (context) => AuthWrapper(), // Tu lógica de autenticación
-        '/login': (context) => LoginPage(), // Pantalla de Login
-        '/home': (context) => HomePage(), // Pantalla principal
+        '/auth': (context) => AuthWrapper(), 
+        '/login': (context) => LoginPage(), 
+        '/home': (context) => HomePage(), 
       },
     );
   }
